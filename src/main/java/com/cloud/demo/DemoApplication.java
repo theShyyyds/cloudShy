fbeabb77c4df45793aa40f90ee1bbb1dd7deca11
LIHONGJIA <Lhj8023gitee>
2022-03-15 10:23:41 +0800

    Initial commit 初始化SpringCloud项目

---
+++ src/main/java/com/cloud/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("cloudShy 启动成功");
+    }
+
+}
